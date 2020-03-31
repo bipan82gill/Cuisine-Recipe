a81a3b32faf14ff69c27fd5f823530bd80d75312
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-03-30 22:50:29 -0400

    Updates

---
+++ precache-manifest.6a3ba01fab17772d3511ee6faaafae9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ada2d7f524faf6903c4d20fd7b9bdd9",
+    "url": "/bipan82gill/Cuisine-Recipe.git/index.html"
+  },
+  {
+    "revision": "8e4356dc05a690c2fa35",
+    "url": "/bipan82gill/Cuisine-Recipe.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1d74c51b0b2dd658a4c",
+    "url": "/bipan82gill/Cuisine-Recipe.git/static/js/2.4a0ae626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bipan82gill/Cuisine-Recipe.git/static/js/2.4a0ae626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4356dc05a690c2fa35",
+    "url": "/bipan82gill/Cuisine-Recipe.git/static/js/main.104ebcd1.chunk.js"
+  },
+  {
+    "revision": "5647769050a301c84db3",
+    "url": "/bipan82gill/Cuisine-Recipe.git/static/js/runtime-main.c5869694.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bipan82gill/Cuisine-Recipe.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
