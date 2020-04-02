9e4244dc2f7aeb4fc4cc1c138408a1be8dbfd753
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-02 19:28:21 -0400

    adding cuisine display component

--- src/Cuisines/Components/CuisineItem.js
+++ src/Cuisines/Components/CuisineItem.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import Card from "../../Shared/Components/UIElement/Card";
+import Button from "../../Shared/Components/UIElement/FormComponents/Button";
 import './CuisineItem.css';
 
 const CuisineItem = props =>{
-    return 
+    return (
+    <li className="cuisine-item">
+        <Card className="cuisine-item__content">
+            <div className ="cuisine-item__image">
+                <img src={props.image} alt={props.title} />
+            </div>
+            <div className ="cuisine-item__info">
+                <h2>{props.title}</h2>
+                <p>{props.recipe}</p>
+            </div>
+            <div className="cuisine-item__actions">
+                <Button inverse>WATCH VIDEO</Button>
+                <Button to={`/cuisines/${props.id}`}>EDIT</Button>
+                <Button danger>DELETE</Button>
+            </div>
+        </Card>
+    </li>
+    )
+
 }
 
 export default CuisineItem;
--- src/Cuisines/Pages/ChefCuisines.js
+++ src/Cuisines/Pages/ChefCuisines.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+
+import CuisineList from '../Components/CuisineList';
+
+const Cuisines=[{
+    id:"CU1",
+    title:"Gulab Jamun",
+    image:"https://baliindiancuisine.com/wp-content/uploads/2015/11/Sanjeev-Kapoor-indian-recipe-of-gulab-jamun-452x262.jpg",
+    recipe:"fjhdjfhejf jehfueirhf hejhferhguerd",
+    creator:"C1"
+}]
 
 const ChefCuisines =() =>{
-return 
+    const chefId = useParams().chefId;
+    const loadedCuisines = Cuisines.filter(cuisine => cuisine.creator === chefId);
+    return <CuisineList items={loadedCuisines} />
 }
 
 export default ChefCuisines;
