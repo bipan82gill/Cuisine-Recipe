bf35ba460227d03637ea40d1d9117344cf20d37a
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-11 16:19:16 -0400

    separating routes into controllers

--- Backend/controllers/cuisines-controllers.js
+++ Backend/controllers/cuisines-controllers.js
@@ -1,59 +1,40 @@
-const uuid = require('uuid/v4');
-
 const HttpError = require('../models/http-error');
 
-let Cuisines =[
-    {
-        id :'C1',
-        title: 'Gulab Jamun',
-        recipe:'shcdvdvdjfheriu',
-        url_video: 'bsdyuewhdcue',
-        creator : 'u1'
+// dummy data to retrieve data for checking routes
+    const Cuisines = [
+        {
+        id: 'c1',
+        title: 'Samosa',
+        recipe : ' shcehrfyuerh dbwhduewhf ',
+        creator: 'u1'
+        }
+    ];
+
+// Function to get Cuisine by Cuisine id 
+const getCuisineById = (req, res, next) => {
+        const cuisineId = req.params.cid;
+    
+        const cuisine = Cuisines.find(cuisine => {
+            return cuisine.id === cuisineId;
+        });
+    
+        if(!cuisine) {
+            throw  new HttpError('Could not find a cuisine for a provided cuisine id.', 404);   
+        }
+    
+        res.json({ cuisine });
     }
-];
-
-const getCuisineById = (req, res, next) =>{
-    const cuisineId = req.params.cid; // cuisine id 
-
-    const cuisine = Cuisines.find(c =>{
-        return c.id === cuisineId;
-    });
-
-    if(!cuisine){
-        throw new HttpError('Could not find a cuisine for this id', 404);
-    }
-    res.json({ cuisine });
-};
-
+// Function to get cuisine by Chef id
 const getCuisineByChefId = (req, res, next) => {
     const chefId = req.params.chefid;
 
-    const cuisine = Cuisines.find(c =>{
-        return c.creator ===chefId;
+    const cuisine = Cuisines.find(cuisine => {
+        return cuisine.creator === chefId;
     });
 
     if(!cuisine){
-        return next(
-            newHttpError('Could not find a cuisine for provided chef id', 404)
-        );
-    }
+        return next(new HttpError('Could not find a cuisine for a provided  chef id.', 404));       
+        }
+    
     res.json({ cuisine });
-};
-
-const createCuisine = (req, res, next) => {
-    const { title, recipe, url_video, creator } = req.body;
-
-    const createCuisine = {
-        id: uuid(),
-        title,
-        recipe,
-        url_video, 
-        creator
-    };
-    Cuisines.push(createCuisine);
-
-    res.status(201).json({ cuisine: createCuisine});
-};
-
-const
-
+    }
--- Backend/routes/cuisine-routes.js
+++ Backend/routes/cuisine-routes.js
@@ -1,45 +1,15 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
-
 const router = express.Router();
-// dummy data to retrieve data for checking routes
-const Cuisines = [
-    {
-    id: 'c1',
-    title: 'Samosa',
-    recipe : ' shcehrfyuerh dbwhduewhf ',
-    creator: 'u1'
-    }
-];
+
+const cuisineControllers = require('../controllers/cuisines-controllers');
+
 // route to get cuisine id 
-router.get('/:cid', (req, res, next) => {
-    const cuisineId = req.params.cid;
-
-    const cuisine = Cuisines.find(cuisine => {
-        return cuisine.id === cuisineId;
-    });
-
-    if(!cuisine) {
-        throw  new HttpError('Could not find a cuisine for a provided cuisine id.', 404);   
-    }
-
-    res.json({ cuisine });
-});
+router.get('/:cid', cuisineControllers.getCuisineById);
 
 // route to get cuisine of specific chef 
-router.get('/chef/:chefid', (req, res, next) => {
-    const chefId = req.params.chefid;
+router.get('/chef/:chefid', cuisineControllers.getCuisineByChefId);
 
-    const cuisine = Cuisines.find(cuisine => {
-        return cuisine.creator === chefId;
-    });
 
-    if(!cuisine){
-        return next(new HttpError('Could not find a cuisine for a provided  chef id.', 404));       
-    }
-    
-    res.json({ cuisine });
-})
 module.exports = router;
 
