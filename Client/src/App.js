a57d6ec47e7ce00bd0b2bd2a8d90f22e09e31752
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-20 19:54:37 -0400

    automatic login and logout

--- Client/src/App.js
+++ Client/src/App.js
@@ -10,25 +10,49 @@ import Auth from './Chef/Pages/Auth';
 import { AuthContext} from './Shared/Context/Auth-context';
 import './App.css';
 
+let logoutTimer;
 const  App = () => {
-  const [token , setToken] = useState(false);
-  const[ chefId, setChefId ]= useState(false);
+  const [ token , setToken ] = useState(false);
+  const [ chefId, setChefId ]= useState(false);
+  const [ tokenExpirationDate, setTokenExpirationDate ] = useState();
   
-    const login = useCallback((chefid, token)=>{
+    const login = useCallback((chefid, token, expirationDate )=>{
     setToken(token);
-    localStorage.setItem('chefData', JSON.stringify({chefId: chefid, token:token}))
     setChefId(chefid);
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      'chefData', 
+       JSON.stringify({
+         chefId: chefid, 
+         token:token,
+         expiration: tokenExpirationDate.toISOString()
+        }))
+   
   },[]);
 
   const logout = useCallback(()=>{
     setToken(null);
     setChefId(null);
+    setTokenExpirationDate(null);
     localStorage.removeItem('chefData');
-  },[]);
+  },[ ]);
+
+  useEffect(() => {
+    if(token, tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+    logoutTimer = setTimeout(logout, remainingTime);
+    }else {
+      clearTimeout(logoutTimer);
+    }
+  },[token, logout, tokenExpirationDate]);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('chefData'));
-    if(storedData && storedData.token){
+    if(storedData && 
+      storedData.token &&
+      new Date(storedData.expiration) > new Date())
+      {
       login( storedData.chefid, storedData.token)
     }
   },[login]);
