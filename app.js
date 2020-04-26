3247b326b0d961fca52afca74fe1b9f460dddc61
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-25 21:09:21 -0400

    ommit headers

--- app.js
+++ app.js
@@ -29,16 +29,16 @@ app.get("/", function(req, res) {
 
 app.use('/uploads/images', express.static(path.join('uploads','images')))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   
-    next();
-  });
+//     next();
+//   });
 app.use('/api/cuisines',cuisineRouter);
 app.use('/api/chefs',chefRouter);
 
