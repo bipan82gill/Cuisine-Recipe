a6a169f636773a5803ec89043876861db00936f5
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-28 00:22:25 -0400

    setup only *  routes

--- app.js
+++ app.js
@@ -22,29 +22,29 @@ if (process.env.NODE_ENV === "production") {
 
 app.use('/uploads/images', express.static(path.join('uploads','images')))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   
-    next();
-  });
+//     next();
+//   });
 
 app.use('/api/cuisines',cuisineRouter);
 app.use('/api/chefs',chefRouter);
 
 
-app.get("/*", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 
-app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route', 404);
-    throw error;
-})
+// app.use((req, res, next) => {
+//     const error = new HttpError('Could not find this route', 404);
+//     throw error;
+// })
 
 app.use((error, req, res, next) => {
     if(req.file){
